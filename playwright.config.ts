c445b8452bd7b6e245bbfc875ac33dca86a493b3
Donny Navarro <donaldjnavarro@gmail.com>
2025-02-27 09:30:38 -0500

    Implemented ESLint for VSCode and resolved remaining issues

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 import { config } from 'dotenv';
 import process from 'process';
-config({ path: './.env' })
+config({ path: './.env' });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
